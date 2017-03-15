7232b343a170e8ef0134ca68e8451d489dc8e979
Teo Sartori <m@sartori.dk>
2017-03-15 14:17:13 +0100

    Fix and clean up.

--- TarStreamArchive.swift
+++ TarStreamArchive.swift
@@ -20,6 +20,7 @@ public class TarStreamArchive  : NSObject {
     public var tarReadStream: InputStream?
     var tarWriteStream: OutputStream?
     
+    var previousEntryFileName: String?
     
     let bytesPerBlock = 512
     let bufferSize: Int
@@ -48,6 +49,39 @@ public class TarStreamArchive  : NSObject {
         tarWriteStream?.open()
     }
     
+    func completeHeader(from headerFields: [TarHeader.Field : String], fileByteSize: Int = 0) -> [TarHeader.Field : String] {
+        /// First we populate the return object with the default header fields.
+        var complete = Dictionary<TarHeader.Field, String>() // shortform errored with "missing context"
+        
+        let mode = headerFields[TarHeader.Field.fileMode] ?? "0"
+        
+        guard let m = mode_t(mode) else { fatalError("invalid filemode") }
+        let fileType = self.modeToFileType(mode: m)
+        
+        complete[TarHeader.Field.fileType] = String(fileType.rawValue, radix: 8)
+        
+        complete[TarHeader.Field.fileMode] = fileType == .directory ? self.dmode : self.fmode
+        
+        complete[TarHeader.Field.fileByteSize] = String(fileByteSize, radix: 8)
+        
+        /// Convert Double time interval to Octal
+        complete[TarHeader.Field.fileModTime] = String(Int(Date().timeIntervalSince1970), radix: 8)
+        
+        //entry.header[TarHeader.Field.linkedFileName] = self.previousEntryFileName
+        
+        complete[TarHeader.Field.magic] = "ustar"
+        
+        complete[TarHeader.Field.version] = "00"
+        
+        /// Then add/override with fields from headerFields
+        for (key, value) in headerFields {
+            complete[key] = value
+            print("key is \(key) and value is \(value) so we get \(complete[key])")
+        }
+        print("complete is \(complete)")
+        return complete
+    }
+    
     /// A convenience method that takes no end handler and thus closes the archive
     /// immediately. It does not return the entry.
     /// This should be queued as an operation in a serial queue to avoid multiple
@@ -57,11 +91,14 @@ public class TarStreamArchive  : NSObject {
     /// subsequent calls to addEntry will. If we didn't order them sequentially we
     /// might end up with an interleaved or scrambled tar archive on the output.
     public func addEntry(header: [TarHeader.Field : String], dataStream: InputStream) {
-        
         /// Asynchronously add the block to the serial queue. The block won't return until done.
         serialQueue.async() {
-            let sema = DispatchSemaphore(value: 0)
+
+            let fileName = header[TarHeader.Field.fileName]
+            
+            let group = DispatchGroup.init()
             var entry = TarEntry()
+            
             
             var actualRead = 0
             
@@ -72,29 +109,15 @@ public class TarStreamArchive  : NSObject {
                 dataStream.close()
                 dataStream.remove(from: .main, forMode: .defaultRunLoopMode)
                 
-                entry.header = header
-                
-                let mode = header[TarHeader.Field.fileMode] ?? "0"
-                
-                guard let m = mode_t(mode) else { fatalError("invalid filemode") }
-                let fileType = self.modeToFileType(mode: m)
-                entry.header[TarHeader.Field.fileType] = String(fileType.rawValue, radix: 8)
-                
-                entry.header[TarHeader.Field.fileMode] = fileType == .directory ? self.dmode : self.fmode
-                
-                entry.header[TarHeader.Field.fileByteSize] = String(actualRead, radix: 8)
-                
-                /// Convert Double time interval to Octal
-                entry.header[TarHeader.Field.fileModTime] = String(Int(Date().timeIntervalSince1970), radix: 8)
-                
-                entry.header[TarHeader.Field.magic] = "ustar"
-                
-                entry.header[TarHeader.Field.version] = "00"
+                entry.header = self.completeHeader(from: header, fileByteSize: actualRead)
                 
                 self.close(entry: entry) {
                     /// Now that we've succefully closed and written the entry to 
                     /// the output stream we can signal this entry is done.
-                    sema.signal()
+                    self.previousEntryFileName = fileName
+                    
+                    group.leave()
+
                 }
             }
             
@@ -102,44 +125,59 @@ public class TarStreamArchive  : NSObject {
             dataStream.on(event: .hasBytesAvailable) {
                 
                 var localDat: [UInt8] = Array(repeating: 0, count: self.bytesPerBlock)
-                actualRead += dataStream.read(&localDat, maxLength: self.bytesPerBlock)
+            
+                let bytesRead = dataStream.read(&localDat, maxLength: self.bytesPerBlock)
+                if bytesRead > 0 {
+                    actualRead += bytesRead
                 
-                /// append the localDat to the entry's payload
-                entry.payload += localDat
+                    /// append the localDat to the entry's payload
+                    entry.payload += localDat
+                    print("payload size \(entry.payload.count)")
+                }
             }
             
+            group.enter()
             dataStream.schedule(in: .main, forMode: .defaultRunLoopMode)
             dataStream.open()
-            
-            /// Wait for the signal that the entry has been written to the output stream.
-            _ = sema.wait(timeout: .distantFuture)
+       
+            group.wait()
+            print("Add Entry Done")
         }
     }
     
-    
     public func addEntry(header: [TarHeader.Field : String], endHandler: VoidFunc? = nil) -> TarEntry {
         print("TarStreamArchive addEntry called.")
+        
+        let fileName = header[TarHeader.Field.fileName]
+        
         var entry = TarEntry()
-        
-        entry.header = header
-        
+
+        entry.header = completeHeader(from: header)
+
+        /// Wrap the user's entry end handler in a closure that is called
+        /// (with an updated entry where extra house keeping has been done)
+        /// when TarEntry's end() is called by the user.
         entry.endHandler = { (updatedEntry: TarEntry) in
-            /// add the end handler to the serial queue so we can be sure it doesn't
-            /// get called before any previous writes are done.
+        
+            self.previousEntryFileName = fileName
+            /// add the call to the close method on the serial queue to ensure it doesn't
+            /// get called before any previously queued writes are done. Pass the user's
+            /// entry end handler through to the close method.
             self.serialQueue.async() {
                 self.close(entry: updatedEntry, completionHandler: endHandler!)
             }
         }
-        
         
         return entry
     }
     
     /// Called when the user is done adding data to the current entry.
     func close(entry: TarEntry, completionHandler: @escaping VoidFunc) {
-
-        var e = entry
-        e.end()
+        
+        /// This is where we end up calling the end twice; once when the user explicitly calls entry.end()
+        /// and once here which is called as part of the TarEntry's end handler.
+        //var e = entry
+        //e.end()
         
         self.write(entry: entry, to: self.tarWriteStream!) {
             completionHandler()
@@ -149,6 +187,7 @@ public class TarStreamArchive  : NSObject {
     /// Called when user is done adding entries to the archive
     public func closeArchive() {
         
+        print("TarStreamArchive close")
         serialQueue.async {
             /// an archive is terminated with two blocks of zeros
             let terminatorData = Array<UInt8>(repeating: 0, count: 2 * 512)
@@ -164,7 +203,6 @@ public class TarStreamArchive  : NSObject {
     func write(entry: TarEntry, to stream: OutputStream, endHandler: @escaping VoidFunc) {
         
         print("TarStreamArchive write called.")
-        print(Thread.callStackSymbols)
 
         /// First write out the header
         let hdr = entry.makeHeaderBlob()
